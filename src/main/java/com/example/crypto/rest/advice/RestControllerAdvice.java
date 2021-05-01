23f9454d1804e800875c7885609d8ae818d49b61
Hans Hendrickx <hendrickxhans88@gmail.com>
2021-05-01 21:12:13 +0200

    addded ExceptionHandler CoinNotFoundException

--- src/main/java/com/example/crypto/rest/advice/RestControllerAdvice.java
+++ src/main/java/com/example/crypto/rest/advice/RestControllerAdvice.java
@@ -1,4 +1,25 @@
 package com.example.crypto.rest.advice;
 
-public class RestControllerAdvice {
+import com.example.crypto.domain.exceptions.CoinNotFoundException;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@ControllerAdvice
+public class RestControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CoinNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    protected void coinNotFoundException(CoinNotFoundException ex, HttpServletResponse response)
+            throws RuntimeException, IOException {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+    }
 }
